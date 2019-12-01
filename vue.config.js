55cf784562af28e9103b18ca33994e7f2d972bf5
wuaster <41026689+wuaster@users.noreply.github.com>
2019-11-30 21:36:18 -0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   transpileDependencies: ["vuetify"],
   assetsDir: "../static",
-  publicPath: "/templates",
+  publicPath: "public/",
   outputDir: path.resolve(__dirname, "public/templates"),
   runtimeCompiler: undefined,
   productionSourceMap: undefined,
